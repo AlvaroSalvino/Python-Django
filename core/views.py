9a22d38530fbf4c4c50d920a717594a5e65e8830
Alvaro Salvino <alvaro.salvino@gmail.com>
2024-01-16 11:46:51 -0300

    .

--- core/views.py
+++ core/views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
-
-def hello(request, nome, idade):
-    return HttpResponse('<h1>Hello {} de {} anos.</h1>'.format(nome, idade))
