d1495a9011a2a3382ab81a8317c437c00846f986
Alvaro Salvino <alvaro.salvino@gmail.com>
2024-03-18 19:11:45 -0300

    .

--- agenda/settings.py
+++ agenda/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = 'django-insecure-j4bgv@f@mm0)z6n6)908+#tt!71!)1o@a9^#$v*5!ggl(#weby
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['maltagesto-iyotfj7i.b4a.run', 'localhost', '127.0.0.1']
 
 # Application definition
 
