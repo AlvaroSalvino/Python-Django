7b00601d3869be77b10f44555c77cd223a91822b
Alvaro Salvino <alvaro.salvino@gmail.com>
2024-03-18 23:06:53 -0300

    .

---
+++ staticfiles/service-worker.js
@@ -0,0 +1,48 @@
+var CACHE_MALTA = 'malta-gestao-cache-v1';
+
+var urlsToCache = [
+  '/',
+  '/static/images/malta.ico',
+  '/static/css/style.css',
+  '/static/css/header.css',
+  '/static/js/main.js',
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Cache aberto');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Cache antigo removido');
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request);
+      }
+    )
+  );
+});
