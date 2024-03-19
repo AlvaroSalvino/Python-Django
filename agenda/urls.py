03bc4d4450582403e26015912e1544d094d901ca
Alvaro Salvino <alvaro.salvino@gmail.com>
2024-03-18 22:58:17 -0300

    .

--- agenda/urls.py
+++ agenda/urls.py
@@ -40,5 +40,6 @@ urlpatterns = [
     path('aceitar_ticket/<int:pk>/', views.aceitar_ticket, name='aceitar_ticket'),
     path('fechar_ticket/<int:pk>/', views.fechar_ticket, name='fechar_ticket'),
     path('area_de_trabalho/', views.area_de_trabalho, name='area_de_trabalho'),
-    path('todos_os_tickets_fechados/', views.todos_os_tickets_fechados, name='todos_os_tickets_fechados')
+    path('todos_os_tickets_fechados/', views.todos_os_tickets_fechados, name='todos_os_tickets_fechados'),
+    path('manifest.json/', name='manifest.json')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
