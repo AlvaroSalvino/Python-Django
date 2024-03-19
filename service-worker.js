462f1c37cb3dff0460d17d8b532f592bdc32e7cf
Alvaro Salvino <alvaro.salvino@gmail.com>
2024-03-19 00:32:39 -0300

    .

--- service-worker.js
+++ service-worker.js
@@ -2,8 +2,8 @@ var CACHE_NAME = 'malta-gestao-cache-v1';
 
 var urlsToCache = [
   '/',
-  'style.css',
-  'header.css',
+  'css/style.css',
+  'css/header.css',
 ];
 
 self.addEventListener('install', function(event) {
