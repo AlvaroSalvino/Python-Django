8d133348acf6caf4d351ead5db02895a46f38175
Alvaro Salvino <alvaro.salvino@gmail.com>
2024-03-18 23:00:29 -0300

    Update urls.py

--- agenda/urls.py
+++ agenda/urls.py
@@ -41,5 +41,5 @@ urlpatterns = [
     path('fechar_ticket/<int:pk>/', views.fechar_ticket, name='fechar_ticket'),
     path('area_de_trabalho/', views.area_de_trabalho, name='area_de_trabalho'),
     path('todos_os_tickets_fechados/', views.todos_os_tickets_fechados, name='todos_os_tickets_fechados'),
-    path('manifest.json/', name='manifest.json')
+    path('manifest.json/')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
