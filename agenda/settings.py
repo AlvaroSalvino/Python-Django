0594dfef7988734e1014a4ca41af06ffb9a4cb74
Alvaro Salvino <alvaro.salvino@gmail.com>
2024-03-18 23:38:42 -0300

    .

--- agenda/settings.py
+++ agenda/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-j4bgv@f@mm0)z6n6)908+#tt!71!)1o@a9^#$v*5!ggl(#weby
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['maltagesto-iyotfj7i.b4a.run', 'node84a.containers.back4app.com', 'node83a.containers.back4app.com', 'localhost', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['maltagesto-iyotfj7i.b4a.run', 'node85a.containers.back4app.com', 'node84a.containers.back4app.com', 'node83a.containers.back4app.com', 'localhost', '127.0.0.1', '0.0.0.0']
 
 CSRF_TRUSTED_ORIGINS = ['https://maltagesto-iyotfj7i.b4a.run']
 
