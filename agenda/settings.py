36a9a5b0bfd0ff0b9e64dab0dabb152cad963ec9
Alvaro Salvino <alvaro.salvino@gmail.com>
2024-03-22 14:57:40 -0300

    .

--- agenda/settings.py
+++ agenda/settings.py
@@ -135,8 +135,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-    '/path/to/other/staticfiles',
+    os.path.join(BASE_DIR, 'static')
 ]
 
 # Default primary key field type
